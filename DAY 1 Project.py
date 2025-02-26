cb1a4d77af3b75bdfa48508eae591f9ddfdb348d
Kontor001 <kingsbrainz05@gmail.com>
2025-02-26 14:33:35 +0100

    Add files via upload

---
+++ DAY 1 Project.py
@@ -0,0 +1,5 @@
+# DAY 1 PROJECT: MINIAPP NAME GENERATOR
+print("Welcome to the MiniApp Name Generator.")
+colour = input("What is your favourite colour?\n")
+pet = input("What is the name of your pet?\n")
+print("Your MiniApp  name could be: " + colour + " " + pet)
